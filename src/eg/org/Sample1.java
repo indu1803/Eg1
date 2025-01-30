106b7e4016fb287e3cd952aa1e1d5e950097852c
indu1803 <indu83663@gmail.com>
2025-01-30 17:28:17 +0530

    Update Sample1.java

--- src/eg/org/Sample1.java
+++ src/eg/org/Sample1.java
@@ -4,7 +4,7 @@ public class Sample1 {
 	
 	public static void main(String[] args)
 	{ 
-		System.out.println("Before method 1")l;
+		System.out.println("Before method 1");
 		System.out.println("Method 1");
 		System.out.println("method 1 updated");
 System.out.println("update 1");
